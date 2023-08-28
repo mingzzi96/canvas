4bd2479bfea0ece24d8cab8ae13bc8ce47eb61ab
mingming <jimin23623@gmail.com>
2023-08-28 19:25:56 +0900

    getBoundingClientRect 메서드 활용하여 그림그리기 기초 완성

--- dist/index.de5c0784.js
+++ dist/index.de5c0784.js
@@ -1,3 +1,52 @@
-console.log("canvas");
+console.log("hi");
+class DrawingBoard {
+    MODE = "NONE";
+    isMouseDown = false;
+    constructor(){
+        this.assignElement();
+        this.initContext();
+        this.addEvent();
+    }
+    assignElement() {
+        this.containerEl = document.getElementById("container");
+        this.canvasEl = this.containerEl.querySelector("#canvas");
+        this.toolbarEl = this.containerEl.querySelector("#toolbar");
+        this.brushEl = this.toolbarEl.querySelector("#brush");
+    }
+    initContext() {
+        this.context = this.canvasEl.getContext("2d");
+    }
+    addEvent() {
+        this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
+        this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
+    }
+    onMouseDown(event) {
+        if (this.MODE === "NONE") return;
+        this.isMouseDown = true;
+        const currentPosition = this.getMousePosition(event);
+        this.context.beginPath();
+        this.context.moveTo(currentPosition.x, currentPosition.y);
+        this.context.lineCap = "round";
+        this.context.strokeStyle = "#000000";
+        this.context.lineWidth = 10;
+    // this.context.lineTo(400, 400);
+    // this.context.stroke();
+    }
+    getMousePosition(event) {
+        const boundaries = this.canvasEl.getBoundingClientRect();
+        return {
+            x: event.clientX - boundaries.left,
+            y: event.clientY - boundaries.top
+        };
+    }
+    onClickBrush(event) {
+        const isActive = event.currentTarget.classList.contains("active");
+        // brush div 안에 i 태그가 눌릴 수 있으니까 currentTarget으로 찾기.
+        this.MODE = isActive ? "NONE" : "BRUSH";
+        this.canvasEl.style.cursor = isActive ? "default" : "crosshair";
+        this.brushEl.classList.toggle("active");
+    }
+}
+new DrawingBoard();
 
 //# sourceMappingURL=index.de5c0784.js.map
